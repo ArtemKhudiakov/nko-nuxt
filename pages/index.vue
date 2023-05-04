e4b86d515929fe34797aadc1f6f26837b662f0a8
ArtemKhudiakov <artemka86ber@gmail.com>
2023-05-04 01:47:02 -0300

    feat: navbar 0.1

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial/>
+  <div>
+    <nav class="navigation">
+      <nuxt-link exact class="navigation__link" to="/">Main</nuxt-link>
+      <nuxt-link class="navigation__link" to="/about">About us</nuxt-link>
+      <nuxt-link class="navigation__link" to="/id">Id</nuxt-link>
+    </nav>
+
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
@@ -7,3 +15,18 @@ export default {
   // name: 'IndexPage'
 }
 </script>
+
+
+<style>
+
+.navigation {
+  display: flex;
+  justify-content: space-evenly;
+  align-content: center;
+  height: 50px;
+  margin: 0 auto;
+  font-size: 20px;
+
+}
+
+</style>
